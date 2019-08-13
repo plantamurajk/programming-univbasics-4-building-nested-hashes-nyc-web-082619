ab3182e4727f0c3e8a2024576baf20bac1423517
John Plantamura <plantamurajk@gmail.com>
2019-08-13 23:40:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,7 @@ def monopoly_with_third_tier
   # complexity. 
     hash = monopoly_with_second_tier
     hash[:railroads][:rent_in_dollars] = {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
+    hash[:railroads][:names] = {}
     return hash
 end
 
