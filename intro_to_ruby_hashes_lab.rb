570a7f6b5f80e4ad150816b1dca5eb5571e5f1e9
John Plantamura <plantamurajk@gmail.com>
2019-08-13 23:53:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,8 +31,8 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  hash = monopoly_with_third_tier[:railroads][:names]
-  hash[:reading_railroad] = {"mortgage_value" => "$100"}
+  hash = monopoly_with_third_tier
+  hash[:railroads][:names][:reading_railroad] = {"mortgage_value" => "$100"}
   hash[:pennsylvania_railroad] = {"mortgage_value" => "$200"}
   hash[:b_and_o_railroad] = {"mortgage_value" => "$400"}
   hash[:shortline_railroad] = {"mortgage_value" => "$800"}
